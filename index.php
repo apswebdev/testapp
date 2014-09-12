78fc5f18cc0e33718f0bba3546e820092ea1fa3b
apswebdev <anwar_saludsong@yahoo.com>
2014-09-12 02:05:12 -0700

    just added basic ie checking

--- index.php
+++ index.php
@@ -7,8 +7,11 @@
 require_once 'includes.php';
 
 /*
- * start processing
+ * start processing, just check if ie lower than 7
  */
-display_screen::start();
-
+if(preg_match('/(?i)msie [1-7]/',$_SERVER['HTTP_USER_AGENT'])){
+    echo "<h3 style='color:white; font-family:arial; float:left; margin:20px'>Sorry for the inconvinience but it is highly recommended to use IE 8 and up for viewing this site.<br>Your current browser does not support the latest standards in IE.<br>Please upgrade your IE or use this suggested browsers:<br>Firefox, Chrome, Opera, and Safari.<br>Thank You and God Bless.<br><br> - Anwar ( website owner and creator )</h3>";    
+} else {
+    display_screen::start();
+}
 ?>
